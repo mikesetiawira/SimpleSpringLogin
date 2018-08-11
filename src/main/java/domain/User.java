a2bda7ae5acc02ec1b8ec4d22fdd835165359c29
Kang Wira <mike.floorwalker93@gmail.com>
2018-08-11 23:50:21 +0700

    initializing gradle and creating domain as well as its repository

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,58 @@
+package domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private String username;
+
+    private String password;
+    private char isDeleted;
+    //the username since the username is actually the email
+    private String name;
+
+    public User(){
+        this.isDeleted = '0';
+    }
+
+    public User(String username, String password, String name) {
+        this.username = username;
+        this.password = password;
+        this.isDeleted = '0';
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public char getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(char isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package repository;
+
+import domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    //find that is not deleted
+    @Query(nativeQuery = true, value ="select * from user " +
+            " where username = :username " +
+            " AND is_deleted = '0' ")
+    User findByUsername(@Param("username")String username);
+
+    @Query(nativeQuery = true, value ="select * from user" +
+            " where username = :username " +
+            " AND password = :password " +
+            " AND is_deleted = '0' ")
+    User findByUsernameAndPassword (@Param("username")String username, @Param("password")String password);
+}
